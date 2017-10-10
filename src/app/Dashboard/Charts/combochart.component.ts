9ffd83c4a611be5e5ed62efd4b8d6cc6eda447b8
Anthony Giretti <anthony.giretti@gmail.com>
2017-10-09 22:58:22 -0400

    parameterize combo chart component

--- src/app/Dashboard/Charts/combochart.component.ts
+++ src/app/Dashboard/Charts/combochart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ComboChartConfig } from './../../Models/ComboChartConfig';
 import { GoogleChartsService } from './../../Services/google-charts.service';
@@ -12,20 +12,14 @@ declare var google: any;
 })
 export class ComboChartComponent implements OnInit {
 
-    private _data = [['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda'],
-                     ['2004/05',  165,      938,         522,             998,           450],
-                     ['2005/06',  135,      1120,        599,             1268,          288],
-                     ['2006/07',  157,      1167,        587,             807,           397],
-                     ['2007/08',  139,      1110,        615,             968,           215],
-                     ['2008/09',  136,      691,         629,             1026,          366]];
-
-    private _config = new ComboChartConfig('Monthly Coffee Production by Country', 'Cups', 'Month');
-
-
+    @Input() data: any[];
+    @Input() config: ComboChartConfig;
+    @Input() elementId: String;
+    
     constructor(private _chartService: GoogleChartsService) {}
 
     ngOnInit(): void {
         
-        this._chartService.BuildComboChart("chart_div", this._data, this._config); 
+        this._chartService.BuildComboChart(this.elementId, this.data, this.config); 
     }
 }
--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { ComboChartConfig } from './../Models/ComboChartConfig';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  title = 'Charts and grid samples';
+  title = 'Reusable charts sample';
+
+  data: any[];
+  config: ComboChartConfig;
+  elementId: String;
+  
+  ngOnInit(): void {     
+    this.data = [['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda'],
+                     ['2004/05',  165,      938,         522,             998,           450],
+                     ['2005/06',  135,      1120,        599,             1268,          288],
+                     ['2006/07',  157,      1167,        587,             807,           397],
+                     ['2007/08',  139,      1110,        615,             968,           215],
+                     ['2008/09',  136,      691,         629,             1026,          366]];
+    this.config = new ComboChartConfig('Monthly Coffee Production by Country', 'Cups', 'Month');
+    this.elementId = "chart_div";
+  }
+
 }
